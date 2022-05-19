e5ae3ea0212d23dddcfaca3aeaf434f686c26af5
Felexonyango <felexonyango19@gmail.com>
2022-05-19 20:16:21 +0300

    callback

--- asynchronous-code/callbacks.js
+++ asynchronous-code/callbacks.js
@@ -5,13 +5,3 @@ function clickHandler() {
 }
 
 btn.addEventListener("click", clickHandler)
-
-addItForMe(5,8, function(res) {
-console.log(res)
-})
-
-function addItForMe(num1, num2, cb) {
-    const result = num1 + num2;
-    cb(result);
-}
-
