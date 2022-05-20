82fd9eda47eb312c886bc4fe3dadeef276930c9e
Felexonyango <felexonyango19@gmail.com>
2022-05-20 14:56:31 +0300

    bubble sort

--- DataStructure/Lesson 5/BubbleSort.js
+++ DataStructure/Lesson 5/BubbleSort.js
@@ -5,9 +5,24 @@
 
 function bubbleSort(arr){
   
+for(let i=0; i<arr.length -1; i++){
 
+for(let j=0; j<arr.length -1 -i ; j++){
+    if(arr[j] > arr[j + 1]){
+        
+    const temp = arr[j];
+    arr[j] = arr[j + 1];
+
+    arr[j + 1] = temp
+   
+
+
+    }
+}
+    
+}
 return arr
 
 
 }
- console.log(bubbleSort([50,23,90,40,10,5]))
+ console.log(bubbleSort([50,23,90,40,10,5,45,21,100,1]))
