134063f1e8455cf5db3358eb80caa8dc0894a898
Felexonyango <felexonyango19@gmail.com>
2022-05-20 12:30:29 +0300

    Binary search

--- DataStructure/math-problem/factorial.js
+++ DataStructure/math-problem/factorial.js
@@ -1,5 +1,9 @@
+
+// a series of numbers in which each number ( Fibonacci number ) 
+//is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
 function factorial(number) {
   let result = 1;
+  
   for (let i = 1; i <= number; i++) {
     result *= i;
   }
