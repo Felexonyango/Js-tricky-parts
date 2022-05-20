a7e24b197794e51aa305ca53779857ae548f7616
Felexonyango <felexonyango19@gmail.com>
2022-05-20 18:12:34 +0300

    array sort

--- DataStructure/Lesson1/Array.js
+++ DataStructure/Lesson1/Array.js
@@ -17,6 +17,46 @@ names.splice(2,1)
 console.log(names)
 
 
- 
+
+//sort arrays 
+const numbers =[50,23,90,40,10,5,45,21,100,1,58,90,32]
+
+//use compareFuc as a callback 
+ numbers.sort(compareFunc)
 
 
+console.log(numbers)
+
+function compareFunc(a,b){
+
+    return a-b
+
+
+}
+
+// using objects to sort products'
+const Products =[
+
+{
+    name:"sukuma",
+    price:30,
+    status:true
+},
+{
+    name:"rice",
+    price:20,
+    status:true
+},
+{
+    name:"Milk",
+    price:70,
+    status:true
+}
+
+]
+
+Products.sort((a,b)=>{
+
+    return a.price -b.price
+})
+console.log(Products)
