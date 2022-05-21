25c5d5999488d38a0e7c9ba0f8bb1ca084729d04
Felexonyango <felexonyango19@gmail.com>
2022-05-21 15:25:48 +0300

    add

--- InterView/main.js
+++ InterView/main.js
@@ -75,7 +75,7 @@ function testVar () {
 // what is prototype based  inheritance
 
 
-// The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects.
+// The Prototypical Inheritance is a feature in javascript used to add methods and properties in objects.
 // It is a method by which an object can inherit the properties and methods of another object.
 
 function Person(email,age){
@@ -149,9 +149,9 @@ function Person(email,age){
      //Question 10
      //What is closure and how to use it
 
-  // A closure is the combination of a function bundled together (enclosed) with references
+  // A closure is the combination of a function bundled together with references
   // to its surrounding state (the lexical environment). 
-
+//used to create self contained modules
 let a =10;
 
 function outer(){
@@ -171,3 +171,94 @@ outer()
 
 //difference between call() and apply()
 //call() pass argument individually while apply() pass arguments as an array
+
+// Question 12
+
+// nul vs undefined
+
+
+// undefined value is value not intialized while 
+// null indicate absence of value though it return type of objects
+
+//QUESTION 13
+
+// Define hoisting 
+
+// Its mechanism where variables  and functions declaration are
+// moved  to the top of the scope before code execution
+
+example
+
+console.log(y)
+var y =10
+
+
+//QUESTION 14
+
+// Use strict  checks if variable is declared using var  or let keyword
+
+//QUESTION 15
+// anonymous function(IIFE) VS Normal function
+
+// IIFE(immediate invocation function  expression)  has no name while normal func has a name
+
+// example of normal function
+
+function foo() {
+  console.log("hhhh");
+}
+// example of IIFE function
+ (function(){
+
+  console.log("hhhh");
+ }) ();
+
+// IIFE used to  avoid collisions  -> example decalaration of variable with same name as the function
+
+// QUESTIONQ 16
+// What are design patterns and most used  design patterns
+
+// Design Patterns are time tested architecture  solutions.
+
+// A reusable solution to a commonly occurring problem within a given 
+// context in software design. 
+
+// Design patterns are formalized best practices that the programmer can use to solve 
+// common problems when designing an application or system.
+
+// 1) module design patterns
+
+// helps in creating self content independent components
+// provides encapsulation and abstraction 
+
+// In react js  design patterns
+
+//  talk of React hooks,
+//  Redux ,
+//  High order components
+
+// talk of MVC in Nodejs 
+
+
+// QUESTIONQ 17
+
+// ways of creating objects in javascript
+
+// 1 Literals
+
+var pat ={
+  name:"Felix",
+  email:"a2@gmail.com"
+}
+
+// 2 object.create
+var  patnew = object.create({name:"",email:""})
+
+// 3 Constructor
+
+function Person(email,age){
+  this.email=email,
+  this.age=age
+}
+ const person= new Person('Felex@gmail.com',24)
+ console.log(person.email)
